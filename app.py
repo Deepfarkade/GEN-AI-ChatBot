e1cc2187d59d0722437413e11e7e6eb5cff1ceec
Deep nilesh farkade <farkadedeep1999@gmail.com>
2024-04-17 22:14:57 +0530

    updated changes as of 17th Mar 24 10:15 PM

--- app.py
+++ app.py
@@ -10,26 +10,26 @@ from langchain.memory import ConversationBufferWindowMemory
 from langchain.schema import AIMessage
 from flask import jsonify
 from jinja2 import Environment, FileSystemLoader
-
-
+ 
+ 
 app = Flask(__name__)
 app.secret_key = 'your-secret-key'  # Set a secret key for session management
-
+ 
 os.environ['REQUESTS_CA_BUNDLE'] = 'cert.crt'
 setup_connexion()
-
+ 
 TF_ENABLE_ONEDNN_OPTS = 0
-
+ 
 # Load spaCy model
 nlp = spacy.load("en_core_web_lg")
-
+ 
 def find_similar_question(query, user_role):
     predefined_questions_file = f"{user_role.lower().replace(' ', '_')}_predefined_questions.json"
-
+ 
     # Load predefined questions from JSON file
     with open(predefined_questions_file, 'r') as f:
         predefined_questions = json.load(f)
-
+ 
     # Iterate through predefined questions to find the most similar one
     max_similarity = 0
     similar_question = None
@@ -38,89 +38,89 @@ def find_similar_question(query, user_role):
         if similarity > max_similarity:
             max_similarity = similarity
             similar_question = question
-    
+   
     return similar_question
-
+ 
 def prompt_next_question(query, user_role):
     similar_question = find_similar_question(query, user_role)
     if similar_question:
         predefined_questions_file = f"{user_role.lower().replace(' ', '_')}_Predefined_questions.json"
-        
+       
         # Check if the predefined questions file exists
         if os.path.exists(predefined_questions_file):
             # Load predefined questions from the appropriate JSON file
             with open(predefined_questions_file, 'r') as f:
                 predefined_questions = json.load(f)
-            
+           
             # Find the index of the similar question
             index = predefined_questions.index(similar_question)
-            
+           
             # Get the next question in the sequence
             if index + 1 < len(predefined_questions):
                 next_question = predefined_questions[index + 1]
             else:
                 # Loop back to the first question if the prompted question is the last question
                 next_question = predefined_questions[0]
-            
+           
             return next_question
     return None
-
-
+ 
+ 
 def save_context(self, input_str, output_str):
     self.chat_memory.add_ai_message(output_str)
-
+ 
 def save_user_question(question, user_role):
     user_role_file_path = f"{user_role.lower().replace(' ', '_')}_questions.json"
-
+ 
     # Load existing questions from file if it exists
     if os.path.exists(user_role_file_path):
         with open(user_role_file_path, "r") as f:
             user_questions = json.load(f)
     else:
         user_questions = {}
-
+ 
     # Update count or add new question
     user_questions[question] = user_questions.get(question, 0) + 1
-
+ 
     # Save updated questions to file
     with open(user_role_file_path, "w") as f:
         json.dump(user_questions, f, indent=2)  # Write dictionary with indentation for readability
-
+ 
 def load_user_questions(user_role):
     user_role_file_path = f"{user_role.lower().replace(' ', '_')}_questions.json"
-
+ 
     if os.path.exists(user_role_file_path):
         with open(user_role_file_path, "r") as f:
             return json.load(f)
     else:
         return {}  # Return an empty dictionary if file doesn't exist
-
+ 
 def get_top_5_questions(user_role):
     user_questions = load_user_questions(user_role)
-
+ 
     # Sort questions by count in descending order
     sorted_questions = sorted(user_questions.items(), key=lambda x: x[1], reverse=True)
-
+ 
     # Return the top 5 questions
     top_5_questions = [question for question, count in sorted_questions[:5]]
     return top_5_questions
-
+ 
 # Initialize conversation history
 memory = ConversationBufferWindowMemory(memory_key="chat_history", k=4, return_messages=True)
-
+ 
 # Check if users.json file exists, if not, create an empty one
 USERS_FILE = "users.json"
 if not os.path.exists(USERS_FILE):
     with open(USERS_FILE, "w") as f:
         json.dump({}, f)
-
+ 
 # Load existing users from the JSON file
 try:
     with open(USERS_FILE, "r") as f:
         users = json.load(f)
 except json.JSONDecodeError:
     users = {}
-
+ 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -134,7 +134,7 @@ def login():
         else:
             return render_template('login.html', error='Invalid username or password')
     return render_template('login.html')
-
+ 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -149,8 +149,9 @@ def signup():
         else:
             return render_template('signup.html', error='Username already exists')
     return render_template('signup.html')
-
+ 
 def process_and_display_sql(question, user_role):
+    table_html = ""  # Initialize table_html with an empty string
     sql = generate_sql_cached(question=question)
     if sql:
         try:
@@ -158,40 +159,42 @@ def process_and_display_sql(question, user_role):
             if not df.empty:
                 # Convert DataFrame to an HTML table with proper formatting
                 table_html = generate_responsive_table(df)
-                #output = f'Bot: Here are the results of the query:<br>{table_html}'
-                output = f'<div class="qtext">Bot: Here are the results of the query:</div><br>{table_html}'
+                output = f'<div class="qtext">Here are the results of the query:</div><br>{table_html}'            
             else:
-                output = "Bot: No data found for the given query."
+                output = "Bot: No data found for the given query. Please reframe your question."
         except Exception as e:
-            output = f"Bot: Error executing SQL query: {str(e)}"
+            output = f"Hey there! 🌟 It seems like I'm not quite equipped to tackle that question just yet. But no worries, our team is hard at work to make sure I can assist you better in the future. Mind reframing your question for now? Thanks a bunch! 🚀"
     else:
-        output = "Bot: Sorry, I couldn't generate a valid SQL query for your question."
-
+        output = "Hey there! 🌟 It seems like I'm not quite equipped to tackle that question just yet. But no worries, our team is hard at work to make sure I can assist you better in the future. Mind reframing your question for now? Thanks a bunch! 🚀"
+ 
     # Save to memory
     memory.save_context({"input": question}, {"output": str(output)})
     # Save user's question to user role-specific file
     save_user_question(question, user_role)
     return output, table_html
-
+ 
 def generate_responsive_table(df):
     table_html = df.to_html(index=False, escape=False)
-    table_html = table_html.replace('<table', 
+    table_html = table_html.replace('<table',
     '<table style="font-size: 12px;text-align:center;border:0;"')
-    table_html = table_html.replace('<tr', 
+    table_html = table_html.replace('<tr',
     '<tr style="padding:10px; text-align: center; text-transform:uppercase;"')
     table_html = table_html.replace('<td', '<td style="padding:10px;text-align: center"')
     # Add responsive table styles
     table_html = f"""
     <div class="table-responsive" style="max-height: 350px;">
+        <div id="copy-button-container">
+            <button onclick="copyToClipboard()">Copy Table</button>
+        </div>
         {table_html}
     </div>
     """
     return table_html
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if request.method == 'GET':
@@ -205,12 +208,12 @@ def chat():
         user_input = data['user_input']
         user_role = session.get('user_role', '')
         output, table_html = process_and_display_sql(user_input, user_role)
-
+ 
         next_question = prompt_next_question(user_input, user_role)
-
+ 
         return jsonify({'output': output, 'table_html': table_html, 'next_question': next_question})
-
-
+ 
+ 
 @app.route('/logout')
 def logout():
     # Clear session_state variables related to chat history
@@ -220,6 +223,6 @@ def logout():
     session.pop('username', None)
     session.pop('user_role', None)
     return redirect(url_for('login'))
-
+ 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOM content loaded. Executing JavaScript...');
   const chatForm = document.getElementById('chat-form');
   const chatHistory = document.querySelector('.chat-history');
+  const nextQuestionElement = document.querySelector('.next-question p');
+  const topQuestionsContainer = document.querySelector('.top-questions');
 
   chatForm.addEventListener('submit', async function(event) {
     event.preventDefault();
@@ -10,7 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('user_input').value = '';
     if (userInput === '') return; // Ignore empty input
 
-    
+    topQuestionsContainer.style.display = 'none';
+
 
     // Add user message to chat history
     appendMessage('user', userInput);
@@ -32,20 +34,26 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       const data = await response.json();
-      console.log('Server response:', data); // Log the entire response object
-      
       // Add bot response to chat history
       appendMessage('bot', data.output);
 
-      // Check if there is a prompted question and display it
+      document.querySelectorAll(".table-responsive th").forEach(headerCell=>{
+        headerCell.addEventListener("click",()=>{
+          const tableElement = headerCell.parentElement.parentElement.parentElement;
+          const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+          const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+          sortTableByColumn(tableElement,headerIndex,!currentIsAscending); 
+        });
+      });      
+
+      // Update next question, if available
       const nextQuestion = data.next_question;
       if (nextQuestion) {
         const promptMessage = "I think you'd like to ask:";
         const clickableQuestion = `<a href="#" class="prompted-question">${nextQuestion}</a>`;
-        appendMessage('bot', `Bot: ${promptMessage} ${clickableQuestion}`);
+        appendMessage('bot', `${promptMessage} ${clickableQuestion}`);
       }
-
-      // Auto-scroll to the bottom of the chat history
+    
       chatHistory.scrollTop = chatHistory.scrollHeight;
     } catch (error) {
       console.error('Error:', error);
@@ -58,8 +66,18 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Event delegation for handling click on prompted questions
-  chatHistory.addEventListener('click', function(event) {
+  document.addEventListener('click', function(event) {
     if (event.target.classList.contains('prompted-question')) {
+      event.preventDefault();
+      // Get the text of the clicked question
+      const questionText = event.target.textContent;
+      // Fill the input field with the clicked question text
+      document.getElementById('user_input').value = questionText;
+    }
+  });
+
+  document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('suggestion')) {
       event.preventDefault();
       // Get the text of the clicked question
       const questionText = event.target.textContent;
@@ -70,18 +88,92 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function appendLoadingMessage() {
     const loadingMessage = document.createElement('div');
-    loadingMessage.classList.add('message2', 'bot', 'loading');
-    loadingMessage.innerHTML = `<div class="loading-spinner"></div> Please wait while we are fetching your answer...`;
+    loadingMessage.classList.add('message', 'bot', 'loading');
+    loadingMessage.innerHTML = `<div class="loading-spinner"></div><div style="padding: 10px;">Please wait while we are fetching your answer...</div>`;
     chatHistory.appendChild(loadingMessage);
     return loadingMessage;
   }
 
+  function copyToClipboard() {
+    var tableHtml = document.querySelector('.table-responsive').innerHTML;
+    var tempInput = document.createElement('textarea');
+    tempInput.value = tableHtml;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Table copied to clipboard!');
+  }
+
+
   function appendMessage(sender, message) {
     const messageElement = document.createElement('div');
     messageElement.classList.add('message', sender);
-    // Use innerHTML instead of textContent to render HTML
-    messageElement.innerHTML = message;
+
+    const iconElement = document.createElement('div');
+    iconElement.classList.add(sender === 'user' ? 'user-icon' : 'bot-icon');
+    iconElement.innerHTML = sender === 'user' ? '<i class="fas fa-user"></i>' : '<i class="fas fa-robot"></i>';
+    messageElement.appendChild(iconElement);
+
+    const contentElement = document.createElement('div');
+    contentElement.classList.add('message-content');
+    contentElement.innerHTML = message; // Use innerHTML to render HTML content
+    messageElement.appendChild(contentElement);
+
     chatHistory.appendChild(messageElement);
   }
-
 });
+
+function sortTableByColumn(table, column, asc = true) {
+  const dirModifier = asc ? 1 : -1;
+  const tbody = table.tBodies[0];
+  const rows = Array.from(tbody.querySelectorAll("tr"));
+
+  // Sort each row
+  const sortedRows = rows.sort((a, b) => {
+    const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent;
+    const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent;
+
+    // Check if it's a date
+    const isDate = !isNaN(Date.parse(aColText)) && !isNaN(Date.parse(bColText));
+    const isNumber = !isNaN(parseFloat(aColText)) && !isNaN(parseFloat(bColText));
+    // If it's a date, convert to Date object for comparison
+    if (isDate) {
+      const dateA = new Date(aColText);
+      const dateB = new Date(bColText);
+      return (dateA - dateB) * dirModifier;
+    } else if (isNumber) {
+      const aValue = parseFloat(aColText);
+      const bValue = parseFloat(bColText);
+      return (aValue - bValue) * dirModifier;
+    } else {
+      // If not a date, compare as text
+      return aColText.localeCompare(bColText) * dirModifier;
+    }
+  });
+
+  // Remove all existing TRs from the table
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  // Readd the sorted rows
+  tbody.append(...sortedRows);
+
+  // Remove existing sorting icons
+  table.querySelectorAll("th").forEach(th => {
+    th.classList.remove("th-sort-asc", "th-sort-desc");
+    th.querySelector('.fa-sort-up')?.remove();
+    th.querySelector('.fa-sort-down')?.remove();
+  });
+
+  // Add sorting icon to the clicked column header
+  const header = table.querySelector(`th:nth-child(${column + 1})`);
+  if (asc) {
+    header.classList.add("th-sort-asc");
+    header.innerHTML += '<i class="fas fa-sort-up"></i> ';
+  } else {
+    header.classList.add("th-sort-desc");
+    header.innerHTML += '<i class="fas fa-sort-down"></i> ';
+  }
+}
